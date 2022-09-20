ec7e7bff356c5f3f5264c05786eb2da95e8dcc66
satrio priambodo <satriopriambodo96@gmail.com>
2022-09-20 17:29:25 +0700

    2nd commit

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
+import Transition from "react-transition-group/Transition";
+import "./Modal.css";
 
-import './Modal.css';
-
-const modal = (props) => (
-    <div className="Modal">
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
-);
+const modal = (props) => {
+  const cssClasses = ["Modal"];
+  return (
+    <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
+      {(state) => {
+        <div className={cssClasses.join(" ")}>
+          <h1>A Modal</h1>
+          <button className="Button" onClick={props.closed}>
+            Dismiss
+          </button>
+        </div>;
+      }}
+    </Transition>
+  );
+};
 
 export default modal;
