2d758aa88a0a426d16582b66b1a3c06fd258a2db
satrio priambodo <satriopriambodo96@gmail.com>
2022-09-21 14:50:55 +0700

    animated

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,20 +1,33 @@
 import React from "react";
-import Transition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 import "./Modal.css";
 
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
+
 const modal = (props) => {
-  const cssClasses = ["Modal"];
   return (
-    <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
-      {(state) => {
-        <div className={cssClasses.join(" ")}>
-          <h1>A Modal</h1>
-          <button className="Button" onClick={props.closed}>
-            Dismiss
-          </button>
-        </div>;
+    <CSSTransition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+      classNames={{
+        enter: "",
+        enterActive: "ModalOpen",
+        exit: "",
+        exitActive: "ModalClosed",
       }}
-    </Transition>
+    >
+      <div className="Modal">
+        <h1>A Modal</h1>
+        <button className="Button" onClick={props.closed}>
+          Dismiss
+        </button>
+      </div>
+    </CSSTransition>
   );
 };
 
